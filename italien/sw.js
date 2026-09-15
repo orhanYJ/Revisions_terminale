1bf490520986fc0125bdb33ac7e81af8035e6847
orhanYJ <orhan@local>
2026-09-15 11:08:23 +0000

    Italien: nouvel onglet Sequenze e orale (methode EO, lexique de la description, Sequenza 1)

--- italien/sw.js
+++ italien/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'italien-v1';
+var CACHE = 'italien-v2';
 var ASSETS = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', function (e) {
